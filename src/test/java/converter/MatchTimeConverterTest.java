94eb32bdfcb519d6d963a39339a3792e938ec4f1
Nikolett Palyi <npalyidev@gmail.com>
2021-10-25 22:11:01 +0100

    adding more unit test

--- src/test/java/converter/MatchTimeConverterTest.java
+++ src/test/java/converter/MatchTimeConverterTest.java
@@ -5,25 +5,23 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.junit.jupiter.api.Test;
 
 class MatchTimeConverterTest {
-    @Test
-    void testConvert() {
-        MatchTimeConverter matchTimeConverter = new MatchTimeConverter();
-        assertTrue(matchTimeConverter.convert(new ArrayList<String>()).isEmpty());
-    }
+  @Test
+  void testConvert() {
+    MatchTimeConverter matchTimeConverter = new MatchTimeConverter();
+    assertTrue(matchTimeConverter.convert(new ArrayList<String>()).isEmpty());
+  }
 
-    @Test
-    void testConvert2() {
-        MatchTimeConverter matchTimeConverter = new MatchTimeConverter();
+  @Test
+  void testConvert2() {
+    MatchTimeConverter matchTimeConverter = new MatchTimeConverter();
 
-        ArrayList<String> stringList = new ArrayList<String>();
-        stringList.add("foo");
-        List<String> actualConvertResult = matchTimeConverter.convert(stringList);
-        assertEquals(1, actualConvertResult.size());
-        assertEquals("INVALID", actualConvertResult.get(0));
-    }
+    ArrayList<String> stringList = new ArrayList<String>();
+    stringList.add("foo");
+    List<String> actualConvertResult = matchTimeConverter.convert(stringList);
+    assertEquals(1, actualConvertResult.size());
+    assertEquals("INVALID", actualConvertResult.get(0));
+  }
 }
-
--- src/test/java/converter/MatchTimeValidatorTest.java
+++ src/test/java/converter/MatchTimeValidatorTest.java
@@ -5,10 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import converter.constants.ConverterConstants;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import org.junit.jupiter.api.Test;
 
 class MatchTimeValidatorTest {
